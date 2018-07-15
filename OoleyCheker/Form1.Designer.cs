2f34e19de5500813449858100946772dd392c7bb
alkihuri <alkihuri@gmail.com>
2018-07-15 15:34:13 +0300

    Intitial commit

---
+++ OoleyCheker/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace OoleyCheker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conect 2 DB";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(106, 38);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(141, 111);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "last id = ";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 98);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Telegram";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 174);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ OoleyCheker/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FirebirdSql;
+using FirebirdSql.Data.FirebirdClient;
+using System.IO;
+using System.Net;
+using System.Web; 
+
+namespace OoleyCheker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            latest_id.Add(0);
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false; 
+        }
+
+        List<int> latest_id = new List<int>();
+
+
+        public void  GetData ()
+        { 
+            File.WriteAllText("lastid", latest_id.Max().ToString() );
+
+            //так проверять состояние соединения (активно или не активно)
+            if (fb.State == ConnectionState.Closed)
+                fb.Open();
+
+            FbTransaction fbt = fb.BeginTransaction(); //стартуем транзакцию; стартовать транзакцию можно только для открытой базы (т.е. мутод Open() уже был вызван ранее, иначе ошибка)
+
+            FbCommand SelectSQL = new FbCommand("select * from FB_EVN FULL OUTER JOIN FB_USR ON FB_EVN.USR = FB_USR.ID; ", fb); //задаем запрос на выборку
+
+            SelectSQL.Transaction = fbt; //необходимо проинициализить транзакцию для объекта SelectSQL
+            FbDataReader reader = SelectSQL.ExecuteReader(); //для запросов, которые возвращают результат в виде набора данных надо использоваться метод ExecuteReader()
+
+            string select_result = ""; //в эту переменную будем складывать результат запроса Select
+
+            try
+            {
+                while (reader.Read()) //пока не прочли все данные выполняем...
+                {
+
+                    ///
+
+                    latest_id.Add(reader.GetInt32(0)); 
+                    select_result = 
+                      //  reader.GetString(0) + "\r\n" 
+                        reader.GetString(1) + "\r\n" 
+                        +reader.GetString(29) + "\r\n<"
+                        + reader.GetString(7) + "<\r\n"
+                        + reader.GetString(2) + "\r\n" +
+                       // reader.GetString(33) + "\r\n"
+
+
+
+                         "\n";
+
+                    select_result = select_result.Replace("000B3A001B7B", " 5этаж");
+                    select_result = select_result.Replace("000B3A001B7A", " 6этаж");
+                    if (reader.GetInt32(0) > Convert.ToInt32(File.ReadAllText("lastid")))
+                    {
+                        label1.Text = "last id = " + latest_id.Max();
+                        richTextBox1.Text = select_result;
+                        if(checkBox1.Checked)
+                        {
+                           if(reader.GetInt32(3)==2)
+                            sendteleg(select_result);
+                        }
+                        File.WriteAllText("lastid", latest_id.Max().ToString());
+                    }
+                    
+                    
+                }
+            }
+            finally
+            {
+                //всегда необходимо вызывать метод Close(), когда чтение данных завершено
+                reader.Close();
+                fb.Close(); //закрываем соединение, т.к. оно нам больше не нужно
+            }
+           // MessageBox.Show(select_result); //выводим результат запроса
+            SelectSQL.Dispose(); //в д
+            
+
+        }
+        /// <summary>
+        /// 000B3A001B7B - 5
+        /// 000B3A001B7A - 6 
+        /// 1518566023167 - камиль 
+        /// </summary>
+
+        FbConnection fb;
+
+        /// <summary>
+        /// 1518623161148
+        /// </summary>
+        /// <param name="line"></param>
+        public void sendteleg(string line)
+        {
+
+            List<string> cheker = GetReq("http://alkihuri.ru/cheker/ooley.txt");         
+            
+            
+                if(!line.Contains("1518623161148")&&cheker[0].Contains("ON"))
+                {
+                line = line.Split('<')[0] + line.Split('<')[2]; 
+                line = Uri.EscapeDataString(line);
+                try
+                {
+                    GetReq("https://api.telegram.org/bot479709612:AAHgac6lvxOV_lSKllOHsZdlBTj4EdISZOE/sendMessage?chat_id=-286482601&text="
+                    + line);
+                }
+                catch (Exception dd)
+                {
+                    GetReq("https://api.telegram.org/bot479709612:AAHgac6lvxOV_lSKllOHsZdlBTj4EdISZOE/sendMessage?chat_id=-286482601&text=ПользовательПрошел!");
+                    File.WriteAllText("data.txt", line + Environment.NewLine);
+
+                }                
+            }           
+        }
+
+        public List<string> GetReq(string url)
+        {
+
+            List<string> resp = new List<string>();
+            WebRequest wrGETURL;
+            wrGETURL = WebRequest.Create(url);
+            wrGETURL.Proxy = WebProxy.GetDefaultProxy();
+            Stream objStream;
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+            StreamReader objReader = new StreamReader(objStream);
+            string sLine = "";
+            int i = 0;
+            while (sLine != null)
+            {
+                i++;
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                    resp.Add(sLine);
+            }
+            return resp;
+        }
+
+
+        public void connectDB()
+        {
+            FbConnectionStringBuilder fb_con = new FbConnectionStringBuilder();
+            fb_con.Charset = "UNICODE_FSS"; //используемая кодировка
+            fb_con.UserID = "SYSDBA";//логин
+            fb_con.Password = "masterkey"; //пароль
+            ///CBASE.FDB GBASE.FDB 
+            fb_con.Database = "C:\\Program Files (x86)\\ENT\\Server\\DB\\CBASE.FDB "; //путь к файлу базы данных
+            fb_con.ServerType = 0; //указываем тип сервера (0 - "полноценный Firebird" (classic или super server), 1 - встроенный (embedded))
+
+            //создаем подключение
+            fb = new FbConnection(fb_con.ToString()); //передаем нашу строку подключения объекту класса FbConnection
+
+            fb.Open(); //открываем БД
+            FbDatabaseInfo fb_inf = new FbDatabaseInfo(fb);
+            this.Text = ("Info: " + fb_inf.ServerClass + "; " + fb_inf.ServerVersion); //выводим тип и версию используемого сервера Firebird
+
+           //информация о БД
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connectDB();
+            //пока у объекта БД не был вызван метод Open() - никакой информации о БД не получить, будет только ошибка
+            // MessageBox.Show("Info: " + fb_inf.ServerClass + "; " + fb_inf.ServerVersion   ); //выводим тип и версию используемого сервера Firebird
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+           
+                while(1==1)
+                    {
+                        GetData();
+                    }
+             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync(); 
+        }
+    }
+
+ 
+}
